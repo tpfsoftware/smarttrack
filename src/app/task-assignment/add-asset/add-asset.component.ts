2ff3d07b01114c8c39923c961edbc8e089511551
shanmugapriya <Shanmugapriya.durairaj@tpfsoftware.com>
2019-11-25 12:22:53 +0530

    css change for bay ad

--- src/app/task-assignment/add-asset/add-asset.component.ts
+++ src/app/task-assignment/add-asset/add-asset.component.ts
@@ -7,7 +7,11 @@ import { MatDialogRef } from '@angular/material';
   encapsulation: ViewEncapsulation.None
 })
 export class AddAssetComponent implements OnInit {
-
+  bayTypes :any=[21,22,24,26];
+  baggage:any=["BT101","BT102","BT103","BT104"];
+  catering:any=["CT101","CT102","CT103"];
+  ladder:any=["ST101","ST102"];
+  bus:any=["BU101"]
   constructor( private dialogRef: MatDialogRef<AddAssetComponent>) { }
 
   ngOnInit() {
